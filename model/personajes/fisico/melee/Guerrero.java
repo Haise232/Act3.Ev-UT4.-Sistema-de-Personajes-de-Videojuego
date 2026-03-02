fdae0e56855efba76bed759e8eb8c436ff21dc8f
Izel Correa Baena <izelcoba@gmail.com>
2026-03-02 14:14:07 +0000

    Eliminar la comprobación de durabilidad redundante en PersonajeFisico y agregar la implementación de la clase Guerrero

---
+++ model/personajes/fisico/melee/Guerrero.java
@@ -0,0 +1,11 @@
+public class Guerrero extends PersoanjeMelee {
+    
+    public Guerrero (String nombre, int nivel, int salud, int estamina, int fuerzaFisica, String tipoArma, int armadura, int agilidad, int peso) {
+        super(nombre, nivel, salud, estamina, fuerzaFisica, tipoArma, armadura, agilidad, peso);
+    }
+
+    @Override
+    public void ataque(Personaje personajeObjetivo) {
+        
+    }
+}
